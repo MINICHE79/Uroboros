83bc44b607cd2cf7cc24d55afab5b82c35ea4491
MINICHE79 <31643234+MINICHE79@users.noreply.github.com>
2018-04-05 07:35:38 -0600

    Avance 05/04/18

--- app/src/main/java/com/project/uroboros/uroboros/App.java
+++ app/src/main/java/com/project/uroboros/uroboros/App.java
@@ -9,11 +9,16 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -37,6 +42,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.jsoup.nodes.Document;
@@ -75,6 +83,9 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
     String photo;
     ImageView profile;
     TextView email;
+    NavigationView navigationView;
+    SharedPreferences preferences;
+    AdView mAdView;
 
 
     @Override
@@ -99,7 +110,7 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         View headerView = navigationView.getHeaderView(0);
 
@@ -118,22 +129,8 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(getResources().getColor(R.color.menu));
 
-
-        /*prueba.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    EI = new ExtractImages(App.this);
-                    //Threads = EI.boards("an");
-                    Images = EI.threads("https://boards.4chan.org/an/thread/2607983");
-                    title = EI.returnTitle();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                Toast.makeText(App.this, "Descargando" + title, Toast.LENGTH_SHORT).show();
-                new DownloadIm().execute();
-            }
-        });*/
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        board = preferences.getString("InitialBoard","/a/");
 
         try {
             ET = new ExtractThreads(App.this);
@@ -213,6 +210,11 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
 
         });
 
+        MobileAds.initialize(this, getResources().getString(R.string.adsId));
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
     }
 
     private void fillImages() {
@@ -223,7 +225,71 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
         }
     }
 
-    public void reLoad(){
+    private void GetSettings(){
+
+
+        //Japanese Culture Boards
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(0).setVisible(preferences.getBoolean("AnimeManga",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(1).setVisible(preferences.getBoolean("AnimeCute",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(2).setVisible(preferences.getBoolean("AnimeWallpapers",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(3).setVisible(preferences.getBoolean("Mecha",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(4).setVisible(preferences.getBoolean("Cosplay",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(5).setVisible(preferences.getBoolean("CuteMale",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(6).setVisible(preferences.getBoolean("Transp",true));
+        navigationView.getMenu().getItem(0).getSubMenu().getItem(7).setVisible(preferences.getBoolean("OtakuCulture",true));
+
+        //Videogames Boards
+        navigationView.getMenu().getItem(1).getSubMenu().getItem(0).setVisible(preferences.getBoolean("Videogames",true));
+        navigationView.getMenu().getItem(1).getSubMenu().getItem(1).setVisible(preferences.getBoolean("VideogamesG",true));
+        navigationView.getMenu().getItem(1).getSubMenu().getItem(2).setVisible(preferences.getBoolean("Pokemon",true));
+        navigationView.getMenu().getItem(1).getSubMenu().getItem(3).setVisible(preferences.getBoolean("RetroG",true));
+
+        //Interest Boards
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(0).setVisible(preferences.getBoolean("Comics",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(1).setVisible(preferences.getBoolean("Technology",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(2).setVisible(preferences.getBoolean("TV",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(3).setVisible(preferences.getBoolean("Weapons",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(4).setVisible(preferences.getBoolean("Auto",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(5).setVisible(preferences.getBoolean("Animals",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(6).setVisible(preferences.getBoolean("TraditionalG",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(7).setVisible(preferences.getBoolean("Sports",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(8).setVisible(preferences.getBoolean("ASports",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(9).setVisible(preferences.getBoolean("ScienceMath",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(10).setVisible(preferences.getBoolean("History",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(11).setVisible(preferences.getBoolean("International",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(12).setVisible(preferences.getBoolean("Outside",true));
+        navigationView.getMenu().getItem(2).getSubMenu().getItem(13).setVisible(preferences.getBoolean("Toys",true));
+
+        //Interest Boards
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(0).setVisible(preferences.getBoolean("Oekaki",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(1).setVisible(preferences.getBoolean("Papercraft",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(2).setVisible(preferences.getBoolean("Photography",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(3).setVisible(preferences.getBoolean("Food",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(4).setVisible(preferences.getBoolean("Artwork",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(5).setVisible(preferences.getBoolean("Wallpapers",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(6).setVisible(preferences.getBoolean("Literature",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(7).setVisible(preferences.getBoolean("Music",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(8).setVisible(preferences.getBoolean("Fashion",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(9).setVisible(preferences.getBoolean("3DCG",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(10).setVisible(preferences.getBoolean("GraphicDesign",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(11).setVisible(preferences.getBoolean("DoIt",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(12).setVisible(preferences.getBoolean("WorksafeGIF",true));
+        navigationView.getMenu().getItem(3).getSubMenu().getItem(13).setVisible(preferences.getBoolean("Quest",true));
+
+        //Other Boards
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(0).setVisible(preferences.getBoolean("Business",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(1).setVisible(preferences.getBoolean("Travel",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(2).setVisible(preferences.getBoolean("Fitness",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(3).setVisible(preferences.getBoolean("Paranormal",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(4).setVisible(preferences.getBoolean("Advice",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(5).setVisible(preferences.getBoolean("LGBT",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(6).setVisible(preferences.getBoolean("Pony",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(7).setVisible(preferences.getBoolean("News",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(8).setVisible(preferences.getBoolean("WorksafeReq",true));
+        navigationView.getMenu().getItem(4).getSubMenu().getItem(9).setVisible(preferences.getBoolean("VIP",true));
+    }
+
+    private void reLoad(){
         lista.clear();
         adapter.notifyDataSetChanged();
         ImgNum = 0;
@@ -251,6 +317,7 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
     }
 
     public void Settings(MenuItem item) {
+        startActivity(new Intent(App.this,SettingsActivity.class));
     }
 
 
@@ -265,7 +332,6 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
                 // Set progress dialog message
                 dialog.setMessage("Loading Threads");
                 dialog.setIndeterminate(false);
-
                 // Show progress dialog
                 dialog.show();
                 loading = false;
@@ -342,6 +408,12 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        GetSettings();
+    }
+
+    @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -350,6 +422,7 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
             super.onBackPressed();
         }
     }
+    
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -370,8 +443,6 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
             board = "/cgl/";
         else if (id == R.id.cm)
             board = "/cm/";
-        else if (id == R.id.f)
-            board = "/f/";
         else if (id == R.id.n)
             board = "/n/";
         else if (id == R.id.jp)
@@ -476,22 +547,7 @@ public class App extends AppCompatActivity implements NavigationView.OnNavigatio
     public boolean onCreateOptionsMenu(Menu menu) {//Menu del toolbar
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_app, menu);
+
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {//Opciones de la toolbar
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
